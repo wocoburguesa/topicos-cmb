b67917405fda403d9567c222f5d433c88b721547
wocoburguesa <wocoburguesa@gmail.com>
2013-04-21 18:07:35 -0500

    Finished simulation

--- network/network.py
+++ network/network.py
@@ -1,60 +1,125 @@
+from pprint import pprint
+
+from poisson import *
+
 class Node(object):
-    
+
     def __init__(self, _id):
         self.id = _id
         self.neighbors = []
-    
+        self.packages = 0
+
     def add_link(self, node):
         if node not in self.neighbors:
             self.neighbors.append(node)
+
+    def add_package(self):
+        self.packages += 1
+
+    def remove_package(self):
+        self.packages -= 1
+
+    def __str__(self):
+        return "<NODE ID=%d PACKAGES=%d>" % \
+            (self.id, self.packages)
+
+class Package(object):
+    def __init__(self, origin, destination, path):
+        self.current = origin
+        self.destination = destination
+        self.path = path
+
+    def __str__(self):
+        return "<PACKAGE CURRENT_POS=%d DESTINATION=%d>" % (
+            self.current, self.destination)
 
 class Network(object):
 
     def __init__(self,
                  nodes,
-                 links):
+                 links,
+                 lamb):
+        self.poisson = Poisson()
+        self.poisson.make_distribution(lamb)
+
         self.nodes = []
         self.checked = []
         for x in range(nodes):
             self.nodes.append(Node(x))
-            
+
         for link in links:
             self.nodes[link[0]-1].add_link(self.nodes[link[1]-1])
             self.nodes[link[1]-1].add_link(self.nodes[link[0]-1])
 
-    def print_all(self):
-        for node in self.nodes:
-            print '%d -> ' % node.id,
-            for neighbor in node.neighbors:
-                print '%d ' % neighbor.id,
-            print
-                
-    def bfs(self, start, end, steps, checked, to_check, path):
-        path = []
-        checked.append(start)
+        self.packages = []
+
+    def step(self):
+        self.generate_packages()
+        for pack in self.packages:
+            if len(pack.path) == 0:
+                self.nodes[pack.current].remove_package()
+
+        self.packages = filter(lambda a: len(a.path) != 0, self.packages)
+
+        for pack in self.packages:
+            self.nodes[pack.current].remove_package()
+            pack.current = pack.path.pop()
+            self.nodes[pack.current].add_package()
+
+    def generate_packages(self, lamb=None):
+        if lamb:
+            self.poisson = Poisson()
+            self.poisson.make_distribution(lamb)
+
+        for i in range(self.poisson.generate()):
+            origin = random.randint(0, len(self.nodes)-1)
+            destination = random.randint(0, len(self.nodes)-1)
+            while origin == destination:
+                destination = random.randint(0, len(self.nodes)-1)
+            path = self.make_path(origin, destination)
+            path.pop()
+            self.packages.append(Package(origin, destination, path))
+
+        for pack in self.packages:
+#            print pack
+#            print pack.path
+            self.nodes[pack.current].add_package()
+
+    def make_path(self, start, end):
+        return self.bfs(start, end, {start: -1}, [])
+
+    def path_from_dict(self, checked, start):
+        path = [start]
+        while checked[start] != -1:
+            path.append(checked[start])
+            start = checked[start]
+        return path
+
+    def bfs(self, start, end, checked, to_check=None):
+        if not to_check:
+            to_check = []
         counter = 0
-        ans = 0
-        print start, 
         for node in self.nodes[start].neighbors:
-            print node.id,
-            if node.id not in checked:
+            if node.id not in checked.keys():
                 counter += 1
-                checked.append(node.id)
-                to_check.append(node.id)
+                checked[node.id] = start
+                if node.id not in to_check:
+                    to_check.append(node.id)
                 if node.id == end:
-                    ans = steps
-        print
+                    return self.path_from_dict(checked, start)
+
         if counter == 0:
-            return counter
-#        for node in self.nodes[start].neighbors:
-#            ans += self.bfs(node.id, end, steps+1, checked)
-        for node in to_check:
-            to_check.remove(node)
-            ans += self.bfs(node, end, steps+1, checked, to_check, path)
-        if ans:
-            path.append(start)
-            return ans
-        else:
-            return 0
+            return None
 
+        for i in range(len(to_check)):
+            next_ = to_check.pop(0)
+            res = self.bfs(next_, end, checked, to_check)
+            if res:
+                return res
 
+    def __str__(self):
+        string =  "<NETWORK>\n"
+        for node in self.nodes:
+            string += "%s\n" % node.__str__()
+        string += "</NETWORK>"
+        return string
--- network/poisson.py
+++ network/poisson.py
@@ -1,36 +1,44 @@
 import math
+import random
 
 POISSON_MIN = 0
 POISSON_MAX = 100
 
-def poisson_function(l, x):
-    if x >= POISSON_MAX:
-        return 0
-    
-    denom = math.factorial(x)
-    num = math.exp(-l) * (l**x)
-    return num / denom
+class Poisson(object):
+    def __init__(self):
+        pass
 
-def poisson_dist(l):
-    intervals = []
-    prev = 0
-    for x in range(POISSON_MAX):
-        intervals.append((
-                x,
-                prev+poisson_function(l, x)
-                ))
-        prev = intervals[x][1]
+    def poisson_function(self, lamb, x):
+        if x >= POISSON_MAX:
+            return 0
 
-    return intervals
+        denom = math.factorial(x)
+        num = math.exp(-lamb) * (lamb**x)
+        return num / denom
 
-def get_element(distribution, prob):
-    buff = distribution[0]
-    for x in distribution:
-        if prob > x[1]:
-            buff = distribution[x[0]+1]
-        else:
-            break
-    return buff[0]
+    def make_distribution(self, lamb):
+        self.distribution = []
+        prev = 0
+        for x in range(POISSON_MAX):
+            self.distribution.append((
+                    x,
+                    prev+self.poisson_function(lamb, x)
+                    ))
+            prev = self.distribution[x][1]
+
+    def get_element(self, prob):
+        buff = self.distribution[0]
+        for x in self.distribution:
+            if prob > x[1]:
+                buff = self.distribution[x[0]+1]
+            else:
+                break
+        return buff[0]
+
+    def generate(self):
+        prob = random.randint(0, 100)
+        prob = float(prob)/100
+        return self.get_element(prob)
 
 if __name__ == '__main__':
     dist = poisson_dist(5)
